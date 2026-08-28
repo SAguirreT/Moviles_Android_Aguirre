cd5c5a3192cc7510e8c1bbe53ae2f146ab585560
Sheila Aguirre <sheila.aguirre@tecsup.edu.pe>
2026-08-28 01:34:59 -0500

    Proyecto inicial con menú del carrito

---
+++ lab02/app/src/main/java/com/aguirre/laboratorio2/Carrito.kt
@@ -0,0 +1,7 @@
+package com.aguirre.laboratorio2
+
+fun main() {
+    println("=========================================")
+    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
+    println("=========================================")
+    }

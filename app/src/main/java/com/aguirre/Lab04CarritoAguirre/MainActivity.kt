d408219ea94a77f24537c93643b8c3d54754536b
Sheila Aguirre <sheila.aguirre@tecsup.edu.pe>
2026-09-19 22:56:55 -0500

    Commit 1-Proyecto Inciial

---
+++ app/src/main/java/com/aguirre/Lab04CarritoAguirre/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.aguirre.Lab04CarritoAguirre
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aguirre.Lab04CarritoAguirre.ui.theme.Lab04CarritoTecuspTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab04CarritoTecuspTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab04CarritoTecuspTheme {
+        Greeting("Android")
+    }
+}

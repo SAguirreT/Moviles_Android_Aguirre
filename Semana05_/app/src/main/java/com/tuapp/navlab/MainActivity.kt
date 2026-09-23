5aed0cc69b259b3e14222d219cbb18359aa13663
Sheila Aguirre <sheila.aguirretecsup.edu.pe>
2026-09-23 07:36:22 -0500

    COMMIT 3- Configuracion y limpieza del MainActivity para establecer el punto de arranque de la navegacion con el archivo AppNavigation

--- Semana05_/app/src/main/java/com/tuapp/navlab/MainActivity.kt
+++ Semana05_/app/src/main/java/com/tuapp/navlab/MainActivity.kt
@@ -3,16 +3,16 @@ package com.tuapp.navlab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.tuapp.navlab.navigation.AppNavigation
-import com.tuapp.navlab.ui.theme.NavLabTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            NavLabTheme {
-                AppNavigation()
-            }
+            // Llama al grafo general de navegación para iniciar el flujo de la app
+            AppNavigation()
         }
     }
 }
